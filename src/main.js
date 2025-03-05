7832d59bea7f1182ee4b32267203cc210202a1d5
Uliana Heron <uheron@Ulianas-MacBook-Air.local>
2025-03-04 21:41:07 -0600

    finished

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-const emailInput = document.getElementById("email");
-const invalidEmailMessage = document.getElementById("invalid-email");
-const form = document.getElementById("form");
-form.addEventListener("submit", submitForm);
+document.addEventListener("DOMContentLoaded", () => {
+  const emailInput = document.getElementById("email");
+  const invalidEmailMessage = document.getElementById("invalid-email");
+  const form = document.getElementById("email-form");
 
-emailInput.addEventListener("invalid", (e) => {
-  invalidEmailMessage.style.display = "flex";
+  if (!form) {
+    console.error("Form not found!");
+    return;
+  }
+
+  form.addEventListener("submit", submitForm);
+
+  emailInput.addEventListener("invalid", (e) => {
+    invalidEmailMessage.style.display = "flex";
+  });
+
+  function submitForm(event) {
+    event.preventDefault();
+    const userEmail = emailInput.value;
+
+    location.replace(
+      "./pages/success-page.html?user-email=" + encodeURIComponent(userEmail)
+    );
+  }
 });
-
-function submitForm(event) {
-  event.preventDefault();
-
-  location.replace("./pages/success-page.html");
-}
